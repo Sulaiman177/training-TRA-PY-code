5564d7bf28562ace57951d7910468737dec3a8a8
unknown <sulaimansaid177@gmail.com>
2026-03-31 11:08:06 +0400

    task 1

--- ip_validator.py
+++ ip_validator.py
@@ -1,8 +1,21 @@
-ipv4=input(" please enter IPv4 address")
+ipv4=input(" please enter IPv4 address:")
 
-parts=ipv4.split(" . ")
+parts=ipv4.split(".")
 
 if len(parts)!=4:
-    print(" invalid IPv4 address format")
+   print(" invalid IPv4 address format")
 else:
-    valid=true
+    valid=True
+    for part in parts:
+        if not part.isdigit():
+          valid=False
+        num=int(part)
+        if num< 0 or num >255:
+          valid=False
+    if valid:
+        print("correct IPv4 format")
+    else:
+        print("invalid IPv4 address format")        
+
+
+
